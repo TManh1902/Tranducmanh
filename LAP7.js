83332bdf5b2d93d1075677ac30e26683fe266246
TManh1902 <134684331+TManh1902@users.noreply.github.com>
2023-06-09 15:16:37 +0700

    Update LAP7.js

--- LAP7.js
+++ LAP7.js
@@ -1,16 +1,30 @@
-function showtip() {
-    var sidtip = document.getElementById("sidtip");
-    sidtip.style.display = "inline";
+function show() {
+    var tooltip = document.getElementById("sidTip");
+    tooltip.style.display = "inline";
 }
 
-function hidetip() {
-    var sidtip = document.getElementById("sidtip");
-    sidtip.style.display = "none";
+function hide() {
+    var tooltip = document.getElementById("sidTip");
+    tooltip.style.display = "none";
 }
 
 function init() {
-    var sid = document.getElementById("sid");
-    sid.onmouseover = showtip;
-    sid.onmouseout = hidetip;
+    var user = document.getElementById("user");
+    user.addEventListener("mouseover", show);
+    user.addEventListener("mouseout", hide);
 }
+
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+});
+
+const form = document.querySelector("form");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
+
 window.onload = init;
